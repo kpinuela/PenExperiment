be8603a04416146c95aead42575ce1a968a4e973
Justin Chan <jtchan2@dons.usfca.edu>
2023-03-31 23:00:40 -0700

    can save IDs from survery

--- pen-experiment/server_items/server.js
+++ pen-experiment/server_items/server.js
@@ -18,6 +18,14 @@ app.listen(8080,()=>{
 
 */
 const http = require('http');
+var survey_ids = new Set();
+const fs = require('fs');
+
+var content = 'This is the text to be written to the file\n';
+
+
+
+
 const server = http.createServer((req, res) => {
     let body = '';
   
@@ -31,7 +39,45 @@ const server = http.createServer((req, res) => {
       console.log("**********");
       if(body.length != 0){
         console.log(item);
+        if(survey_ids.has(item) == false){
+          //only adds to file if survey has the specific number
+          var temp= item.toString() + "\n";
+          fs.access('Qualtrics_IDs.txt', fs.constants.F_OK, (err) => {
+          if (err) {
+            console.log('File does not exist');
+            fs.writeFile('Qualtrics_IDs.txt', temp,function(err){
+              if (err) throw err;
+              console.log("Content written to created file");
+              })
+          } else {
+            console.log('File exists');
+            fs.appendFile("Qualtrics_IDs.txt", temp, function (err) {
+              if(err) throw err;
+              console.log("Content appended to file");
+            })
+          }
+          });
+        }
+        survey_ids.add(item);
       }
+      
+      //can currently add to a file but adds each entry mutliple times
+      for (const entry of survey_ids) {
+        console.log("Set has this item:");
+        console.log(entry);
+        
+        /*
+        fs.appendFile('myfile.txt', entry, err => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log('File has been written!');
+        });
+        */
+      }
+      
+      
       
       
       // Process the data here
