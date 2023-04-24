cc7aacb28b79a8dbdf41eeac5f8df4120ef076ba
tlin41390 <tlin41@dons.usfca.edu>
2023-04-24 16:00:51 -0700

    implemented multiplayer support.

--- pen-experiment/server_items/index.js
+++ pen-experiment/server_items/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 let currCircle = null;
 let timer = 60;
-let players= {};
+let rooms= {};
 
 let score = 0;
 const io = new Server(server, {
@@ -20,16 +20,6 @@ const io = new Server(server, {
     },
 });
 
-const startTimer = (room) => {
-    const intervalId = setInterval(() => {
-        timer--;
-        if (timer === 0) {
-            clearInterval(intervalId);
-        }
-        io.emit("timer", timer);
-    }, 1000);
-}
-startTimer();
 const generateCircle = (room) => {
     const newCircle = {
         x: Math.floor(30 + Math.random() * 60),
@@ -39,57 +29,101 @@ const generateCircle = (room) => {
     };
     currCircle = newCircle;
     io.to(room).emit("current_circle", currCircle);
+
+};
+
+const startTimer = () => {
+    const intervalId = setInterval(() => {
+        timer--;
+        if (timer === 0) {
+            clearInterval(intervalId);
+        }
+    }, 1000);
 }
 
-io.on("connection", (socket) => {
 
+io.on("connection", (socket) => {
     //make a player object for each users
-    const player ={
-        id: socket.id,
-        score: 0,
+    const player = {
+      id: socket.id,
+      score: 0,
     };
-    players[socket.id] = player;
+
     let availablerooms = null;
     io.sockets.adapter.rooms.forEach((room, roomId) => {
-        if (roomId.startsWith('room-') && room.size < 2) {
-            availablerooms = roomId;
-        }
+      if (roomId.startsWith("room-") && room.size < 2) {
+        availablerooms = roomId;
+      }
     });
 
+
+    io.to(availablerooms).emit("initial_score", score);
+  
     if (!availablerooms) {
-        availablerooms = `room-${Date.now()}`;
-        socket.join(availablerooms);
-    }else{
-        socket.join(availablerooms);
-        io.to(availablerooms).emit("start_game", true);
+      availablerooms = `room-${Date.now()}`;
+      socket.join(availablerooms);
+  
+      const room = {
+        players: [],
+        timerStarted: false,
+      };
+      room.players.push(player);
+      rooms[availablerooms] = room;
+    } else {
+      socket.join(availablerooms);
+      rooms[availablerooms].players.push(player);
+      // set opponent
+      rooms[availablerooms].players.forEach((p) => {
+        if (p.id !== player.id) {
+          player.opponent = p.id;
+          p.opponent = player.id;
+        }
+      });
+      io.to(availablerooms).emit("start_game", true);
+    }
+  
+    socket.emit("room_id", availablerooms, socket.id);
+  
+    const clients = io.sockets.adapter.rooms.get(availablerooms);
+    const numClients = clients ? clients.size : 0;
+
+    if(numClients === 2){
+        generateCircle(availablerooms);
+        socket.to(availablerooms).emit("start_game", true);
     }
 
-    socket.emit("room_id", availablerooms);
-
-    generateCircle(availablerooms);
-
+  
     socket.on("circle_clicked", () => {
-        if (currCircle) {
-            currCircle = null;
-            io.emit("update_score", score++);
-            generateCircle(availablerooms);
-        }
-    }
-    );
-
-    socket.on("disconnect", () => {
-        console.log(`User disconnected: ${socket.id}`);
-        if(io.sockets.adapter.rooms.get(availablerooms) == null){
-            console.log(`Room ${availablerooms} is empty`);
-            io.of('/').adapter.rooms.delete(availablerooms);
-        }
+        player.score++;
+      if (currCircle) {
+        currCircle = null;
+        // update score
+        io.to(player.id).emit("update_score",player.id,player.score);
+        // update opponent's score
+        const opponent = rooms[availablerooms].players.find(
+          (p) => p.id === player.opponent
+        );
+          io.to(opponent.id).emit("update_opp_score",player.id,player.score);
+        generateCircle(availablerooms);
+      }
     });
-    console.log("Players:")
-    for(const player in players){
-        console.log(players[player]);
-    }
-})
+  
+    socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.id}`);
+      if (io.sockets.adapter.rooms.get(availablerooms) == null) {
+        console.log(`Room ${availablerooms} is empty`);
+        io.of("/").adapter.rooms.delete(availablerooms);
+        clearInterval(timer);
+      }
+    });
+    console.log("Rooms:");
+    console.log(rooms[availablerooms]);
 
-server.listen(3001, () => {
+    if (numClients === 2 && !rooms[availablerooms].timerStarted) {
+        rooms[availablerooms].timerStarted = true;
+    }
+  });
+  
+  server.listen(3001, () => {
     console.log("listening on 3001");
 });
--- pen-experiment/src/pages/game.jsx
+++ pen-experiment/src/pages/game.jsx
@@ -4,20 +4,28 @@ import io from 'socket.io-client';
 const socket = io.connect("http://localhost:3001");
 
 const Game = (props) => {
+  let initialScore = 0;
+  socket.on("initial_score", (data) => {
+    initialScore = data;
+  });
   //const location = useLocation();
   //const { timer, give, take, request } = location.state;
-  const [score, setScore] = useState(0);
-  const [oppScore,setOppScore] = useState(0);
+  const [score, setScore] = useState(initialScore);
+  const [oppScore, setOppScore] = useState(initialScore);
   const [currentCircle, setCurrentCircle] = useState(null);
   const [gameOver, setGameOver] = useState(false);
-  const [ready,setReady] = useState(false);
+  const [ready, setReady] = useState(false);
   const [timeLeft, setTimeLeft] = useState(60);
-  const [roomId,setRoomId] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [playerId, setPlayerId] = useState("");
+  const [oppId, setOppId] = useState("");
+  const [enabled,setClickEnabled] = useState(true);
 
-  socket.on("room_id", (data) => {
+  socket.on("room_id", (data, player) => {
+    console.log(`player id ${player}`);
     setRoomId(data);
+    setPlayerId(player);
   });
-
 
   // Generate a new circle when there is no current circle
   useEffect(() => {
@@ -29,15 +37,34 @@ const Game = (props) => {
     }
   }, [currentCircle, gameOver]);
 
-  // Handle circle clicks
   const handleClick = () => {
     setCurrentCircle(null);
-    socket.on("update_score", (data) => {
-      setScore(data);
+    socket.emit("circle_clicked", currentCircle);
+    socket.on("update_score", (data, score) => {
+      setScore(score);
+    })
+    console.log(timeLeft);
+  }
+
+  useEffect(() => {
+    socket.on("update_opp_score", (data, score) => {
+      if (data !== playerId) {
+        console.log(`opp id ${data}}`);
+        console.log(score);
+        setOppScore(score);
+      }
+    })
+  });
+  /* Handle circle clicks
+  const handleClick = () => {
+    setCurrentCircle(null);
+    socket.on("update_score", (score) => {
+      setScore(score);
     });
     socket.emit("circle_clicked",currentCircle);
     console.log(timeLeft);
   };
+  */
 
   // End the game when the timer runs out
   useEffect(() => {
@@ -48,15 +75,18 @@ const Game = (props) => {
 
   // Decrement the timer every second
   useEffect(() => {
-    if (timeLeft > 0 && !gameOver) {
-      const timerId = setTimeout(() => {
-        socket.on("timer", (data) => {
-        setTimeLeft(data);
-        });
-      }, 1000);
-      return () => clearTimeout(timerId);
+    socket.on("start_game", (start) => {
+      setReady(start);
+    })
+    if (ready) {
+      if (timeLeft > 0 && !gameOver) {
+        const timerId = setTimeout(() => {
+          setTimeLeft(timeLeft - 1);
+        }, 1000);
+        return () => clearTimeout(timerId);
+      }
     }
-  }, [timeLeft, gameOver]);
+  }, [timeLeft, gameOver,ready]);
 
   return (
     <div className="formContainer">
@@ -75,8 +105,8 @@ const Game = (props) => {
             <div
               style={{
                 position: 'absolute',
-                top: currentCircle.y +`%`,
-                left: currentCircle.x +`%`,
+                top: currentCircle.y + `%`,
+                left: currentCircle.x + `%`,
                 width: currentCircle.radius * 2,
                 height: currentCircle.radius * 2,
                 borderRadius: '100%',
