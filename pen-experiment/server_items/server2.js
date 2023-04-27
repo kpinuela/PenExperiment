9dcddb9c6cb9e1a3444fa415c369389475ebc416
Justin Chan <jtchan2@dons.usfca.edu>
2023-04-26 22:14:53 -0700

    modded server to use express isntead of http

--- pen-experiment/server_items/server2.js
+++ pen-experiment/server_items/server2.js
@@ -8,9 +8,6 @@ const randomInt = Math.floor(Math.random() * (max - min + 1) + min);
 
 var content = 'This is the text to be written to the file\n';
 const supabaseUrl = 'https://xjpvvjxoyqhwdfqjsflk.supabase.co';
-//this key beolow should not be shared publically only privavetly, it bypass RLS
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcHZ2anhveXFod2RmcWpzZmxrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTQ0ODI1MCwiZXhwIjoxOTk3MDI0MjUwfQ.QEh_IRx17HaP4ET1ZwEzWFKKfEHAea7giF_17LZxIXU
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcHZ2anhveXFod2RmcWpzZmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0NDgyNTAsImV4cCI6MTk5NzAyNDI1MH0.Bfuvo_ahI2VGNdcBdDMIC2Cnt0WpfIBRGfrZ03bi98w
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcHZ2anhveXFod2RmcWpzZmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0NDgyNTAsImV4cCI6MTk5NzAyNDI1MH0.Bfuvo_ahI2VGNdcBdDMIC2Cnt0WpfIBRGfrZ03bi98w';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -63,50 +60,15 @@ const server = http.createServer( async (req, res) => {
         if(survey_ids.has(item) == false){
           //only adds to file if survey has the specific number
           insertGameInfo(item);
+          survey_ids.add(item);
           var temp= item.toString() + "\n";
-          fs.access('Qualtrics_IDs.txt', fs.constants.F_OK, (err) => {
-          if (err) {
-            console.log('File does not exist');
-            fs.writeFile('Qualtrics_IDs.txt', temp,function(err){
-              if (err) throw err;
-              console.log("Content written to created file");
-              })
-          } else {
-            console.log('File exists');
-            fs.appendFile("Qualtrics_IDs.txt", temp, function (err) {
-              if(err) throw err;
-              console.log("Content appended to file");
-            })
-          }
-          });
         }
         survey_ids.add(item);
       }
       
       //can currently add to a file but adds each entry mutliple times
-      for (const entry of survey_ids) {
-        console.log("Set has this item:");
-        console.log(entry);
-        
-        /*
-        fs.appendFile('myfile.txt', entry, err => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log('File has been written!');
-        });
-        */
-      }
       updateIDItems();
       iterateGameInfo();
-      
-      
-      
-      
-      // Process the data here
-      //res.writeHead(200, {'Content-Type': 'text/plain'});
-      //res.end('Data received');
     });
   });
   
